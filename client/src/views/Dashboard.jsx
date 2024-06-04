0bf1c0a68184cb07d22ad11470d23834f86d214e
Moises Gomez <moisesgomezr9@gmail.com>
2024-06-04 16:52:42 -0700

    synchronized the react-calendar with the react-big-calendar. Now when navigating the react-big-calendar using the drill up, down, next buttons, the react-calendar will change view accordinly depending on whether it is a new month. this was done by creating a handleActiveDateChange function and using the onActiveStartDateChange prop

--- client/src/views/Dashboard.jsx
+++ client/src/views/Dashboard.jsx
@@ -1,31 +1,42 @@
-import React, { useState, useCallback } from "react";
+import React, { useState, useCallback, useEffect } from "react";
 import '../styles/dashboard.css'
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 
 import BigCalendar from "../components/BigCalendar";
 
-
-
 const Dashboard = () => {
-
+    // value state variable is set to the current date initially. setValue is used to update this date.
     const [value, setValue] = useState(new Date());
+    //currently disabling navigation, need to see what side effects are overwritting 
+    const [activeStartDate, setActiveStartDate] = useState(new Date());
+
+    // handleDateChange function updates the date state with the new date selected on the react-calendar.
     const handleDateChange = (date) => {
-        setValue (date);
+        setValue(date);
+         //currently disabling navigation, need to see what side effects are overwritting 
+        setActiveStartDate(date);
     }
 
-    const onNavigate = useCallback((newDate) => setValue(newDate), [setValue])
-    return (<div className="dashboard-container">
-        <div>
-        <Calendar onChange={handleDateChange} value={value} />
-       
-        </div>
-        <BigCalendar date={value} onNavigate={onNavigate}/>
+    // onNavigate is a memoized callback that updates the date state. This function is passed to BigCalendar to handle navigation changes.
+    const onNavigate = useCallback((newDate) => {
+        setValue(newDate);
+         //currently disabling navigation, need to see what side effects are overwritting 
+        setActiveStartDate(newDate);
+    }, [setValue, setActiveStartDate]);
 
+    const handleActiveDateChange = ({ activeStartDate }) => {
+        setActiveStartDate(activeStartDate);
+    };
+    
+    return (
+        <div className="dashboard-container">
+            <div>
+                <Calendar onChange={handleDateChange} value={value} activeStartDate={activeStartDate} onActiveStartDateChange={handleActiveDateChange}  />
+            </div>
+            <BigCalendar date={value} onNavigate={onNavigate} />
         </div>
     );
-
 }
-
 
 export default Dashboard;
