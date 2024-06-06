7eb63852c6f0080a7aeaef2a8c3c33c8bb76999f
Eleanor <LNRfrye@gmail.com>
2024-06-05 18:28:18 -0700

    changed LogIn to component, imported into Landing view

--- client/src/components/logIn.jsx
+++ client/src/components/logIn.jsx
@@ -1,25 +1,16 @@
-import React, { useState, useCallback, useEffect } from "react";
-import Modal from "react-modal";
+import React from 'react';
 
-const LogInModal = () => {
+const LogIn = () => {
     return (
-        <Modal
-        isOpen={modalIsOpen}
-        onAfterOpen={afterOpenModal}
-        onRequestClose={closeModal}
-        style={customStyles}
-        contentLabel="Log-in Modal"
-      >
-        <h2 ref={(_subtitle) => (subtitle = _subtitle)}>Sign Up</h2>
-        <button onClick={closeModal}>close</button>
-        <div>This is the sign up modal</div>
-        <form id="logIn" action="/logIn" method="POST">
-            <input id="user" name="user" placeholder="user" type="text"></input>
-            <br></br>
-            <input id="pass" name="pass" placeholder="password" type="text"></input>
-            <br></br>
-            <button id="submit" type="submit">Log in!</button>
-        </form>
-      </Modal>
-    )
+        <div>
+            <h2>Login</h2>
+            <form id="logIn" action="/logIn" method="POST">
+                <input id="user" name="user" placeholder="user" type="text"></input>
+                <input id="pass" name="pass" placeholder="password" type="text"></input>
+                <button id="submit" type="submit">Log in!</button>
+            </form>
+        </div>
+    );
 };
+
+export default LogIn;
