422baa3c2b7fa3b3ef727b41705cf57d62c1ec7b
Eleanor <LNRfrye@gmail.com>
2024-06-05 19:33:58 -0700

    Sign Up

--- client/src/views/Landing.jsx
+++ client/src/views/Landing.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
-import LogIn from '../components/LogIn.jsx';
+import LogIn from '../components/logIn.jsx'; // i changed the file name to capitalize it, but it causes errors when i try to import, not sure why?
+import SignUp from '../components/SignUp.jsx';
+
+// create click handler for sign up button to open modal:
 
 const Landing = () => {
-
+    //this returns the login field
     return (
-        <LogIn></LogIn>
+        <div>
+             <LogIn></LogIn>
+             <p>Don't have an account yet?</p> <button>sign up!</button>
+        </div>
+
     )
 }
