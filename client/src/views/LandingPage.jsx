ccfc0ddb421a065f6f6cabc8a4d648fc7b0d50e4
RococoKid <sara.r.kalkstein@gmail.com>
2024-06-05 21:27:35 -0400

    set up landing page style sheet

--- client/src/views/LandingPage.jsx
+++ client/src/views/LandingPage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Benefits from '../components/landingPageComponents/Benefits';
 import NavBar from '../components/landingPageComponents/NavBar';
 import Testimonials from '../components/landingPageComponents/Testimonials';
+import '../styles/landingpage.css';
 
 const LandingPage = () => {
     return (
