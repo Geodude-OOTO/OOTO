59b116cde33e48f93821d87e50bbabe6d2a7f620
RococoKid <sara.r.kalkstein@gmail.com>
2024-06-05 21:37:31 -0400

    tile styling

--- client/src/components/landingPageComponents/Testimonials.jsx
+++ client/src/components/landingPageComponents/Testimonials.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Testimonials = () => {
     return (
-        <div>
+        <div className="tile-container">
             <h2>Here is what our customers say about OOTO</h2>
             <div>
                 <p>With a hybrid work schedule, it can be hard to see who is working, 
@@ -11,14 +11,14 @@ const Testimonials = () => {
                 <h3>Michael</h3>
                 <h4>Regional Manager</h4>
             </div>
-            <div>
+            <div className="tile-container">
                 <p>My manager always forgets when I am traveling in a different timezone for work. 
                     Now with OOTO I do not get calls in the middle of the night!
                 </p>
                 <h3>Jim</h3>
                 <h4>International Paper Salesman</h4>
             </div>
-            <div>
+            <div className="tile-container">
                 <p>I broke my leg which meant I had to be on short-term disability. 
                     With OOTO my team can tell the difference between when I am on sick leave not working, 
                     vs. when I am working from home.
