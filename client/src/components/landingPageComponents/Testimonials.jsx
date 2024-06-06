8a364f01543661b00fe0b311b27de356c1659f00
RococoKid <sara.r.kalkstein@gmail.com>
2024-06-05 22:09:00 -0400

    trying images

--- client/src/components/landingPageComponents/Testimonials.jsx
+++ client/src/components/landingPageComponents/Testimonials.jsx
@@ -8,6 +8,7 @@ const Testimonials = () => {
                 <p>With a hybrid work schedule, it can be hard to see who is working, 
                     but now with OOTO I know that my team is hard at work wherever they are.
                 </p>
+                <img src=".../assets/avatar.png"/>
                 <h3>Michael</h3>
                 <h4>Regional Manager</h4>
             </div>
@@ -15,6 +16,7 @@ const Testimonials = () => {
                 <p>My manager always forgets when I am traveling in a different timezone for work. 
                     Now with OOTO I do not get calls in the middle of the night!
                 </p>
+                <img src=".../assets/avatar-1.png"/>
                 <h3>Jim</h3>
                 <h4>International Paper Salesman</h4>
             </div>
@@ -23,6 +25,7 @@ const Testimonials = () => {
                     With OOTO my team can tell the difference between when I am on sick leave not working, 
                     vs. when I am working from home.
                 </p>
+                <img src=".../assets/avatar-2.png"/>
                 <h3>Dwight</h3>
                 <h4>Assistant to the Regional Manager</h4>
             </div>
