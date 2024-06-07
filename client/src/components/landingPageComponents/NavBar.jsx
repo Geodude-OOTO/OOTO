6d77699b7cf601118db2a537b860dddeb2e4ca88
RococoKid <sara.r.kalkstein@gmail.com>
2024-06-06 21:02:14 -0400

    nav bar

--- client/src/components/landingPageComponents/NavBar.jsx
+++ client/src/components/landingPageComponents/NavBar.jsx
@@ -2,7 +2,10 @@ import React from "react";
 
 const NavBar = () => {
     return (
-        <div>This is the Nav Bar</div>
+        <div>
+            <img src=".../assets/logo.svg"/>
+            <button>This button will be where the login component is.</button>
+        </div>
     )
 };
 
