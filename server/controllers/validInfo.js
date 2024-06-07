a2dd0d0cbc06ff052f4a3d7ea5b127cc74074baa
TaylorBC <taylor.brian99@yahoo.com>
2024-06-06 17:56:56 -0700

    finished login and user register functionality

---
+++ server/controllers/validInfo.js
@@ -0,0 +1,26 @@
+const validInfo = (req, res, next) => {
+    const { email, name, password } = req.body;
+  
+    function validEmail(userEmail) {
+      return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(userEmail);
+    }
+  
+    if (req.path === "/register") {
+      console.log(!email.length);
+      if (![email, name, password].every(Boolean)) {
+        return res.status(400).json("Missing Credentials");
+      } else if (!validEmail(email)) {
+        return res.status(400).json("Invalid Email");
+      }
+    } else if (req.path === "/login") {
+      if (![email, password].every(Boolean)) {
+        return res.status(400).json("Missing Credentials");
+      } else if (!validEmail(email)) {
+        return res.status(400).json("Invalid Email");
+      }
+    }
+  
+    next();
+};
+
+module.exports = validInfo;
