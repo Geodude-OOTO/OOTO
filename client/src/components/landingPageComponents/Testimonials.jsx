d1b9292b5cb61c593107f9f2cb0f05d3b554a5f1
RococoKid <sara.r.kalkstein@gmail.com>
2024-06-07 23:37:37 -0400

    installed file-loader to fix importing images

--- client/src/components/landingPageComponents/Testimonials.jsx
+++ client/src/components/landingPageComponents/Testimonials.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
+import avatar from '../../assets/avatar.png';
+import avatar1 from '../../assets/avatar-1.png';
+import avatar2 from '../../assets/avatar-2.png';
+
 
 
 const Testimonials = () => {
     return (
         <div>
             <h2>Here is what our customers say about OOTO</h2>
+            <div className='testimonial-container'>
             <div className="tile-container">
                 <p>With a hybrid work schedule, it can be hard to see who is working, 
                     but now with OOTO I know that my team is hard at work wherever they are.
                 </p>
-                <img src='.../assets/avatar.png'/>
+                <img src={avatar} />
                 <h3>Michael</h3>
                 <h4>Regional Manager</h4>
             </div>
@@ -17,7 +22,7 @@ const Testimonials = () => {
                 <p>My manager always forgets when I am traveling in a different timezone for work. 
                     Now with OOTO I do not get calls in the middle of the night!
                 </p>
-                <img src=".../assets/avatar-1.png"/>
+                <img src={avatar1} />
                 <h3>Jim</h3>
                 <h4>International Paper Salesman</h4>
             </div>
@@ -26,9 +31,10 @@ const Testimonials = () => {
                     With OOTO my team can tell the difference between when I am on sick leave not working, 
                     vs. when I am working from home.
                 </p>
-                <img src=".../assets/avatar-2.png"/>
+                <img src={avatar2} />
                 <h3>Dwight</h3>
                 <h4>Assistant to the Regional Manager</h4>
+            </div>
             </div>
         </div>
     )
