262414d94e588fafae8632307538524fbd8dae50
Moises Gomez <moisesgomezr9@gmail.com>
2024-06-08 12:55:44 -0700

    working on login

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -44,44 +44,76 @@ const userRegister = async(req, res, next) => {
 } 
 
 
-const userLogin = async (req, res, next) => {
- try {
+// const userLogin = async (req, res, next) => {
+//  try {
  
-    //1. destructure the req.body 
-    const {email, password} = req.body;
+//     //1. destructure the req.body 
+//     const {email, password} = req.body;
 
-    //2. check if the user doesn't exist (if not throw error) 
-    const user = await pool.query("SELECT * FROM activeUsers WHERE user_email = $1", [
-        email 
-        
-    ]);  
-     console.log(user.rows[0]);
-    if(user.rows.length === 0) {
-        return res.status(400).json({message: 'Password or Email is incorrect'});
-    }
+//     console.log (req.body);
 
-    //3. check if incoming password is the same as the database password  
-    const validPassword = await bcrypt.compare(password, user.rows[0].user_password); 
-    //console.log(validPassword)
+//     //2. check if the user doesn't exist (if not throw error) 
+//     const user = await pool.query("SELECT * FROM activeUsers WHERE user_email = $1", [
+//         email 
+//     ]);  
+//      console.log('response from login query',user.rows[0]);
+//     if(user.rows.length === 0) {
+//         return res.status(400).json({message: 'Password or Email is incorrect'});
+//     }
 
-    if(validPassword) { 
-        const token = jwtGenerator(user.rows[0].user_id);  
-        res.redirect('http://localhost:3000/dashboard');
-    } else {
+//     //3. check if incoming password is the same as the database password  
+//     const validPassword = await bcrypt.compare(password, user.rows[0].user_password); 
+//     //console.log(validPassword)
+
+//     if(validPassword) { 
+//         const token = jwtGenerator(user.rows[0].user_id);  
+//         res.redirect('http://localhost:3000/dashboard');
+//     } else {
+//       next(error);
+//     }
+//     //4. give them the jwt token
+//     // const token = jwtGenerator(user.rows[0].user_id);  
+//     // //res.cookie('user_id', user.rows[0].user_id); 
+//     // console.log(email, password, 'line 69')
+//     // res.json({ token }); 
+//     next();
+//  } catch(error) {
+//   console.error(error.message);  
+//   res.status(400).send('server error'); 
+//   next(error);
+//  }
+// }
+
+const userLogin = async (req, res, next) => {
+  try {
+      // Destructure the req.body
+      const { email, password } = req.body;
+
+      console.log('Login Request Body:', req.body);
+
+      // Check if the user doesn't exist (if not, throw error)
+      const user = await pool.query("SELECT * FROM activeUsers WHERE user_email = $1", [email]);
+      console.log('Response from login query:', user.rows[0]);
+
+      if (user.rows.length === 0) {
+          return res.status(400).json({ message: 'Password or Email is incorrect' });
+      }
+
+      // Check if incoming password is the same as the database password
+      const validPassword = await bcrypt.compare(password, user.rows[0].user_password);
+      
+      if (validPassword) {
+          const token = jwtGenerator(user.rows[0].user_id);
+          res.status(200).json({ success: true, message: 'Request processed successfully'});
+      } else {
+          return res.status(400).json({ message: 'Password or Email is incorrect' });
+      }
+  } catch (error) {
+      console.error('Error in userLogin:', error.message);
+      res.status(500).send('Server error');
       next(error);
-    }
-    //4. give them the jwt token
-    // const token = jwtGenerator(user.rows[0].user_id);  
-    // //res.cookie('user_id', user.rows[0].user_id); 
-    // console.log(email, password, 'line 69')
-    // res.json({ token }); 
-    next();
- } catch(error) {
-  console.error(error.message);  
-  res.status(400).send('server error'); 
-  next(error);
- }
-}
+  }
+};
 
 module.exports = {
     userRegister, 
